0277005d2b70f79c5c24da50619e8db5163bf14a
Raul Santos <raul.soutos96@gmail.com>
2020-06-22 23:59:14 -0300

    creating .net project

---
+++ DigitalInnocationOne/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DigitalInnocationOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numDeVezes = 5;
+
+            for (int i = 0; i < numDeVezes; i++)
+            {
+                Console.WriteLine($"What a course {i}");       
+            }
+        }
+    }
+}
