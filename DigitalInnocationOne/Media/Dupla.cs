5dfa899b5fe1dbb80c90d46ad1088fed850fc017
Raul Santos <raul.soutos96@gmail.com>
2020-06-24 00:34:16 -0300

    Applying C# first steps concepts - DIO

---
+++ DigitalInnocationOne/Media/Dupla.cs
@@ -0,0 +1,13 @@
+namespace DigitalInnocationOne.Media
+{
+    public class Dupla
+    {
+        public string nome;
+        public decimal nota;
+        
+        public Dupla(string nome, decimal nota){
+            this.nome = nome;
+            this.nota = nota;
+        }
+    }
+}
--- DigitalInnocationOne/Program.cs
+++ DigitalInnocationOne/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using DigitalInnocationOne.Media;
 
 namespace DigitalInnocationOne
 {
@@ -6,12 +7,41 @@ namespace DigitalInnocationOne
     {
         static void Main(string[] args)
         {
-            int numDeVezes = 5;
-
-            for (int i = 0; i < numDeVezes; i++)
+            Dupla[] duplas = new Dupla[10];
+            int size = 0;
+            string tmp;
+            int opt = 0;
+            while(opt != 4)
             {
-                Console.WriteLine($"What a course {i}");       
-            }
+                Console.WriteLine("Teacher's Options");
+                Console.WriteLine("1 - Add Student");
+                Console.WriteLine("2 - List Students");
+                Console.WriteLine("3 - See the average");
+
+                tmp = Console.ReadLine();
+                opt = Int32.Parse(tmp);
+            //Adicionar      
+                if(opt == 1)
+                {  
+                    string nome = Console.ReadLine();
+                    decimal nota = decimal.Parse(Console.ReadLine());
+                    duplas[size] = new Dupla(nome, nota);
+                    size++;
+                    Console.WriteLine("Name added sucessfully");
+                }
+            //Ler todos os nomes
+                if(opt == 2)
+                {
+                    Console.WriteLine("Nome(s):");
+                    for (int i = 0; i < size; i++)
+                    {
+                        if(duplas[i].nome != null)
+                        Console.WriteLine($"{duplas[i].nome}");
+                    }     
+                }
+                
+            }    
         }
+          
     }
 }
