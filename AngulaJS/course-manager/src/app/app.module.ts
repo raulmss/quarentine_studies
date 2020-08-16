80d8e2d744d37218782c39130406132f844aeb50
Raul Santos <raul.soutos96@gmail.com>
2020-08-15 22:20:13 -0300

    adding nav-bar and standard routers (' ' & '**')

--- AngulaJS/course-manager/src/app/app.module.ts
+++ AngulaJS/course-manager/src/app/app.module.ts
@@ -1,23 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {FormsModule} from  '@angular/forms';
+import {RouterModule} from  '@angular/router';
 
 
 import { AppComponent } from './app.component';
 import { CourseListComponent } from './courses/course-list.component';
 import {StarComponent} from './star/star.component';
 import {ReplacePipe} from './pipe/replace.pipe'
+import {NavBarComponent} from './nav-bar/nav-bar.compoment'
+import {Error404Component} from './404/error404.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     CourseListComponent,
     StarComponent,
-    ReplacePipe
+    ReplacePipe,
+    NavBarComponent,
+    Error404Component
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: '', redirectTo: 'courses', pathMatch: 'full' //o path raiz irá direcionar para courses
+      },
+      {
+        path: 'courses', component: CourseListComponent
+      },
+      {
+        path: '**', component: Error404Component
+      }
+
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ AngulaJS/course-manager/src/app/nav-bar/nav-bar.compoment.ts
@@ -0,0 +1,9 @@
+import {Component, TemplateRef} from '@angular/core'
+
+@Component({
+    selector: 'app-nav-bar',
+    templateUrl: './nav-bar.compoment.html'
+})
+export class NavBarComponent {
+
+}
